8f64d2cc4b0078598c6928c80099d272e6cb55a0
VishalvvSharma26 <vishalxyztech01@gmail.com>
2025-04-01 10:20:43 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,15 @@
+//console.log(addWithArrow(6,7));n
+export const add=(a,b)=>{
+    return a+b;
+}
+export const  sub = (a,b)=>{
+    return a-b;
+}
+//module.exports = {add,sub};
+//module exports = sub;
+
+//odule .exports = addWithArrow;
+
+//function addWithArrow(a,b){
+ /// /  return a+b;
+//}np, 
