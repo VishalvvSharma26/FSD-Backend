0a70d8d5fcad942aa6c19504c4a87c25b73c82ee
VishalvvSharma26 <vishalxyztech01@gmail.com>
2025-04-15 09:35:52 +0530

    Add files via upload

---
+++ filecallback.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+const callbackWriter=()=>{
+fs.writeFile(".dataCallback.csv","This is the callback file",(err)=>{
+    if(err){
+        console.log("Unable to write (callback)",err);
+    }else{
+        console.log("File has been writen successfully.(callback)");
+    }
+    }
+)
+}
+const callbackReader =() =>{
+    fs.readFile(".dataCallback.csv","utf-8",(err,data)=>{
+if(err){
+    console.log("unable to read data.(callback)",err);
+
+}else{
+    console.log("Data is:",data);
+}
+})
+ }
+
+export default {callbackWriter,callbackReader};
